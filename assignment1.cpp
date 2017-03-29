8c9e5d18962d00bcb071a366376974172de90e9c
Pang <psu43@uclive.ac.nz>
2017-03-30 00:49:27 +1300

    rainbow..

--- assignment1.cpp
+++ assignment1.cpp
@@ -625,6 +625,50 @@ void drawSwirls(float radius) {
 }
 
 //------- Ring ----------------------------------------------------
+// A half ring
+void halfRing(float radius)
+{
+    float angle1,angle2, ca1,sa1, ca2,sa2;
+    float x1,z1, x2,z2, x3,z3, x4,z4;  //four points of a quad
+    float toRad = 3.14159265/180.0;  //Conversion from degrees to radians
+    float height = 0.2; // was 1
+    float width = 0.05; // was .5
+    glBegin(GL_QUADS);
+    for(int i = 0; i < 180; i += 5)    //5 deg intervals
+    {
+        angle1 = i * toRad;       //Computation of angles, cos, sin etc
+        angle2 = (i+5) * toRad;
+        ca1=cos(angle1); ca2=cos(angle2);
+        sa1=sin(angle1); sa2=sin(angle2);
+        x1=(radius-width)*sa1; z1=(radius-width)*ca1;
+        x2=(radius+width)*sa1; z2=(radius+width)*ca1;
+        x3=(radius+width)*sa2; z3=(radius+width)*ca2;
+        x4=(radius-width)*sa2; z4=(radius-width)*ca2;
+        
+        glNormal3f(0., height, 0.);       //Quad 1 facing up
+        glVertex3f(x1, height, z1);
+        glVertex3f(x2, height, z2);
+        glVertex3f(x3, height, z3);
+        glVertex3f(x4, height, z4);
+        
+        glNormal3f(-sa1, 0.0, -ca1);   //Quad 2 facing inward
+        glVertex3f(x1, 0.0, z1);
+        glVertex3f(x1, height, z1);
+        glNormal3f(-sa2 ,0.0, -ca2);
+        glVertex3f(x4, height, z4);
+        glVertex3f(x4, 0.0, z4);
+        
+        glNormal3f(sa1, 0.0, ca1);   //Quad 3 facing outward
+        glVertex3f(x2, height, z2);
+        glVertex3f(x2, 0.0, z2);
+        glNormal3f(sa2, 0.0, ca2);
+        glVertex3f(x3, 0.0, z3);
+        glVertex3f(x3, height, z3);
+    }
+    glEnd();
+}
+
+//------- Ring ----------------------------------------------------
 // A single circular ring of specified radius
 void ring(float radius)
 {
@@ -984,11 +1028,12 @@ void drawParabola() {
     }
     glEnd();
     
-
+    
+    
     
 }
 
-void drawRainbow() {
+void drawRainbow2() {
     glPushMatrix();
     glColor4f (0, 1, 0.2, 1.0);
     glTranslatef(0, 0, -20);
@@ -1007,6 +1052,22 @@ void drawRainbow() {
 //    glScalef(0.1, 0.1, 2);
 //    glutSolidCube(1);
 //    glPopMatrix();
+    
+}
+
+void drawRainbow() {
+    glPushMatrix();
+    glTranslatef(0, 0, -20);
+    glRotatef(90, 0, 1, 0);
+    glRotatef(90, 0, 0, 1);
+    halfRing(21);
+    halfRing(20);
+    halfRing(19);
+    halfRing(18);
+    halfRing(17);
+    halfRing(16);
+    halfRing(15);
+    glPopMatrix();
     
 }
 
@@ -1200,7 +1261,7 @@ void display()
 //    glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, lgt1_dir); //light1 direction
 //    glPopMatrix();
     
-    
+
     // draw rover moving
     glPushMatrix();
     glTranslatef(rover_move_x - 25, rover_move_y, rover_move_z - 10);
